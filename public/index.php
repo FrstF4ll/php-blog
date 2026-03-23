bb489bb8cf9948753770f40352ed8c0837434ba4
FrstF4ll <hydralixe@gmail.com>
2026-03-23 13:52:48 +0100

    chore: create base html
    Create css file.

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <title>My PHP blog</title>
+</head>
+<body>
+<h1>Hello world !</h1>
+</body>
+</html>
