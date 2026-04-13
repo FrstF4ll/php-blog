7bbc4042f5cc963a76f4652404686b39557e8f5e
FrstF4ll <hydralixe@gmail.com>
2026-04-13 13:12:32 +0200

    feat: implement custom logo on auth pages

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,7 +1,7 @@
 <?php ?>
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"
+            <img src="assets/favicon.svg" alt="Logo"
                  class="mx-auto h-10 w-auto"/>
             <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
         </div>
