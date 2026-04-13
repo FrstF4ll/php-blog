cc4c6ce636ddb3efa1927552691f50df27458722
FrstF4ll <hydralixe@gmail.com>
2026-04-13 16:22:38 +0200

    refactor: move image source

--- views/pages/create_post.php
+++ views/pages/create_post.php
@@ -2,7 +2,7 @@
 
 ?>
 
-<form class="flex flex-col p-8">
+<form action="index.php" method="POST" class="flex flex-col p-8">
     <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12">
             <h2 class="text-3xl font-semibold text-gray-900 text-center">Create your article</h2>
@@ -29,11 +29,7 @@
                     <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Cover photo</label>
                     <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                         <div class="text-center">
-                            <svg viewBox="0 0 24 24" fill="currentColor" data-slot="icon" aria-hidden="true"
-                                 class="mx-auto size-12 text-gray-300">
-                                <path d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-                                      clip-rule="evenodd" fill-rule="evenodd"/>
-                            </svg>
+                    <img src="/assets/layout.svg">
                             <div class="mt-4 flex text-sm/6 text-gray-600">
                                 <label for="file-upload"
                                        class="relative cursor-pointer rounded-md bg-transparent font-semibold text-indigo-600 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-600 hover:text-indigo-500">
@@ -51,7 +47,7 @@
                 <button type="button" class="rounded-md border-1 border-gray-400 px-3 py-2 text-sm shadow-sm hover:bg-gray-200 font-semibold text-gray-900">Cancel</button>
                 <button type="submit"
                         class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                    Save
+                    Publish
                 </button>
             </div>
         </div>
