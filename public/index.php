350cdbb3f2e4f690e5e3479810418dda7986751d
FrstF4ll <hydralixe@gmail.com>
2026-04-13 12:56:57 +0200

    feat: update main page style
    Correct 'sign up' button color
    
    Add new custom logo for the navbar and the favicon
    
    Change color to mix more between neutral color (such as black and gray) and the indigo business color

--- public/index.php
+++ public/index.php
@@ -19,7 +19,13 @@ $templates = $pages[$request] ?? null;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/svg+xml" href="assets/favicon.svg">
     <title>php-blog - Home</title>
 </head>
 <body class="grid grid-rows-[auto_1fr_auto] min-h-full">
--- views/components/navbar.php
+++ views/components/navbar.php
@@ -7,7 +7,7 @@ $link_state = [
 ?>
 
 
-<nav class="relative bg-gray-800">
+<nav class="relative bg-gray-900">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -15,7 +15,7 @@ $link_state = [
             <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div class="flex shrink-0 items-center">
                     <a href="?pages=home"><img
-                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                                src="assets/logo.svg"
                                 alt="Your Company" class="h-8 w-auto"/>
                     </a>
                 </div>
@@ -37,7 +37,7 @@ $link_state = [
                     Log in
                 </a>
                 <a href="?pages=register"
-                   class="text-sm font-medium text-blue-800 bg-blue-200 hover:bg-blue-100 px-4 py-2 rounded-lg transition-colors">
+                   class="text-sm font-medium text-indigo-800 bg-indigo-200 hover:bg-indigo-100 px-4 py-2 rounded-lg transition-colors">
                     Sign up
                 </a>
             </div>
