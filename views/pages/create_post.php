709e2fe8e59ed75050ff266310f8b060b2ead869
FrstF4ll <hydralixe@gmail.com>
2026-04-14 10:17:58 +0200

    feat: add 'required' to input fields
    handle user empty inputs

--- views/pages/create_post.php
+++ views/pages/create_post.php
@@ -4,15 +4,17 @@
 
 <form method="POST" class="flex flex-col p-8">
     <div class="space-y-12">
+
         <div class="border-b border-gray-900/10 pb-12">
             <h2 class="text-3xl font-semibold text-gray-900 text-center">Create your article</h2>
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
                 <div class="sm:col-span-4">
                     <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                     <div class="mt-2">
                         <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                             <input type="text" name="title" placeholder="Your Article..."
-                                   class="block min-w-0 w-full grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
+                                   class="block min-w-0 w-full grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required />
                         </div>
                     </div>
                 </div>
@@ -42,7 +44,9 @@
                         </div>
                     </div>
                 </div>
+
             </div>
+
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <button type="button" class="rounded-md border-1 border-gray-400 px-3 py-2 text-sm shadow-sm hover:bg-gray-200 font-semibold text-gray-900">Cancel</button>
                 <button type="submit"
@@ -50,5 +54,6 @@
                     Publish
                 </button>
             </div>
+
         </div>
 </form>
