94634133c94802d60ef37213245a860ed950a43a
FrstF4ll <hydralixe@gmail.com>
2026-04-14 13:41:22 +0200

    fix: improve error message on database connection
      Update label for content field
    
      Update gitignore

--- config/db.php
+++ config/db.php
@@ -6,6 +6,6 @@ $pdo = new PDO('sqlite:' . $dbPath);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 return $pdo;
 } catch (PDOException $e) {
-    die("Connection failed : " . $e->getMessage());
+    die("Connection failed.  ");
 }
 ?>
--- views/pages/create_post.php
+++ views/pages/create_post.php
@@ -21,7 +21,7 @@
                 </div>
 
                 <div class="col-span-full">
-                    <label for="about" class="block text-sm/6 font-medium text-gray-900">Content</label>
+                    <label for="content" class="block text-sm/6 font-medium text-gray-900">Content</label>
                     <div class="mt-2">
                         <textarea name="content" rows="3"
                                   class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
