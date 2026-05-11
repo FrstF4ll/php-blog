25f82ff01e3b64ca0f9a8163e99565e1c49ac194
FrstF4ll <hydralixe@gmail.com>
2026-05-11 08:34:53 +0200

    feat(users): add user route

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,6 +7,9 @@ use Frstf4ll\PhpBlog\Post\PostFileUploader;
 use Frstf4ll\PhpBlog\Post\PostRepository;
 use Frstf4ll\PhpBlog\Post\PostService;
 use Frstf4ll\PhpBlog\Post\PostValidation;
+use Frstf4ll\PhpBlog\User\UserController;
+use Frstf4ll\PhpBlog\User\UserRepository;
+use Frstf4ll\PhpBlog\User\UserService;
 
 $validator = new PostValidation();
 $uploader = new PostFileUploader();
@@ -17,7 +20,12 @@ $postController = new PostController($postService);
 
 $pageController = new PageController();
 
+$userRepository = new UserRepository($pdo);
+$userService = new UserService($userRepository);
+$userController = new UserController($userService);
+
 return [
     'PageController' => $pageController,
-    'PostController' => $postController
+    'PostController' => $postController,
+    'UserController' => $userController
 ];
