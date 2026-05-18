d133b3436514d2b1385daa713de94758b4f0d15f
FrstF4ll <hydralixe@gmail.com>
2026-05-18 15:56:38 +0200

    feat: hide section depending on status
    If user is logged in, he will see 'manage your posts' and 'create your own'. If he's not, he'll not see 'manage your posts', and create your own will redirect him to the login page.

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -16,28 +16,34 @@ $link_state = [
             </div>
             <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div class="flex shrink-0 items-center">
-                    <a href="?pages=home"><img
-                                src="assets/logo.svg"
-                                alt="Your Company" class="h-8 w-auto"/>
+                    <a href="?pages=home"><img src="assets/logo.svg" alt="Your Company" class="h-8 w-auto"/>
                     </a>
                 </div>
                 <div class="hidden sm:ml-6 sm:block">
                     <div class="flex space-x-4">
                         <a href="?pages=home" aria-current="page"
                            class="<?= $base_link ?> <?= $link_state['active'] ?>">Home</a>
-                        <a href="?pages=create" class="<?= $base_link ?> <?= $link_state['inactive'] ?>">Create your
-                            own</a>
-                        <a href="?pages=manage" class="<?= $base_link ?> <?= $link_state['inactive'] ?>">Manage your
-                            posts
-                        </a>
+                        <a href=<?= empty($_SESSION['id']) ? "?pages=login" : "?pages=create" ?> class="<?= $base_link ?> <?= $link_state['inactive'] ?>
+                        ">Create your
+                        own</a>
+                        <?php if (!empty($_SESSION['id'])): ?>
+                            <a href="?pages=manage" class="<?= $base_link ?> <?= $link_state['inactive'] ?>">Manage your
+                                posts
+                            </a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
             <?php if (!empty($_SESSION['id'])): ?>
-                <form action="?pages=logout" method="POST" class="text-white right-0 flex items-center gap-3 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($_SESSION['csrf_token'] ?? '') ?>">
+                <form action="?pages=logout" method="POST"
+                      class="text-white right-0 flex items-center gap-3 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <input type="hidden" name="csrf_token"
+                           value="<?= htmlspecialchars($_SESSION['csrf_token'] ?? '') ?>">
                     <p><?= $_SESSION['name'] ?></p>
-                    <button type="submit" class="text-sm font-medium text-red-800 bg-red-200 hover:bg-red-100 px-4 py-2 rounded-lg transition-colors">Log out</button>
+                    <button type="submit"
+                            class="text-sm font-medium text-red-800 bg-red-200 hover:bg-red-100 px-4 py-2 rounded-lg transition-colors">
+                        Log out
+                    </button>
                 </form>
             <?php else: ?>
                 <div class="absolute inset-y-0 right-0 flex items-center gap-3 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
