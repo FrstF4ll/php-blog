107fd11490d6254da4a5908a755d8ec1eab1492c
FrstF4ll <hydralixe@gmail.com>
2026-05-26 10:33:57 +0200

    refactor: implement new status management on users.
    close #12

--- src/User/UserController.php
+++ src/User/UserController.php
@@ -2,32 +2,47 @@
 
 namespace Frstf4ll\PhpBlog\User;
 
-class UserController
+use Frstf4ll\PhpBlog\BaseController;
+use Frstf4ll\PhpBlog\ServiceException;
+
+class UserController extends BaseController
 {
     public function __construct(private readonly UserService $userService)
     {
     }
 
-    public function store(array $userData): array
+    public function store(array $userData): void
     {
         $name = $userData['name'];
         $email = $userData['email'];
         $password = $userData['password'];
         $confirmPassword = $userData['password_confirm'];
+        try {
+            $this->userService->register($name, $email, $password, $confirmPassword);
+            $this->flash('success', 'Registration successful, you can now log in.');
+            $this->redirect('?pages=login');
 
-        return $this->userService->register($name, $email, $password, $confirmPassword);
+        } catch (ServiceException $e) {
+            $this->flash('error', $e->getMessage());
+            $this->redirect('?pages=register');
+        }
     }
 
-    public function authenticateSession(array $userData): array
+    public function authenticateSession(array $userData): void
     {
         $email = $userData['email'] ?? '';
         $password = $userData['password'] ?? '';
-        $result = $this->userService->login($email, $password);
-        if($result['success']){
+        try {
+            $result = $this->userService->login($email, $password);
             $_SESSION['id'] = $result['id'];
             $_SESSION['name'] = $result['name'];
+            $this->flash('success', 'Login successful.');
+            $this->redirect('?pages=home');
+
+        } catch (ServiceException $e) {
+            $this->flash('error', $e->getMessage());
+            $this->redirect('?pages=login');
         }
-        return $result;
     }
 
     public function renderUser(int $id)
